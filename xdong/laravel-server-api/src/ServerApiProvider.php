5da319727c050826b5768c82aacc643d42c7a50e
git <xdongvvv@163.com>
2021-08-10 17:23:36 +0800

    init

---
+++ xdong/laravel-server-api/src/ServerApiProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Xdongvvv\ServerApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServerApiProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
